db18c57526dccb44d8fe34c0eb9af28dc23f1c42
pixee <help@openpixee.io>
2023-01-10 21:02:00 +0000

    Updated Foo.java:5 - Hardened this system call to make it resistant to injected commands and commands that target sensitive files
    [Rules]
     - pixee:java/harden-process-creation

--- src/main/java/org/springframework/samples/petclinic/Foo.java
+++ src/main/java/org/springframework/samples/petclinic/Foo.java
@@ -1,7 +1,8 @@
+import io.pixee.security.SystemCommand;
 import java.io.BufferedReader;
 
 public class Foo {
   public void foo(BufferedReader reader, String c) {
-    Runtime.getRuntime().exec(c);
+    SystemCommand.runCommand(Runtime.getRuntime(), c);
   }
 }
